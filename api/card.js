043ebd897889826ef3a9ddbfaca61c67044d3f20
Karl STEIN <karl.stein.pro@gmail.com>
2015-10-12 13:28:58 -1000

    - moved CardRegistration in MangoPay.cardRegistration - fixed the code that sends card details in MangoPay.card.create()

--- api/card.js
+++ api/card.js
@@ -4,9 +4,12 @@ MangoPaySDK.card = {
      * Types of card
      */
     type: {
-        diners: 'DINERS',
-        maestro: 'MAESTRO',
-        visaMastercard: 'CB_VISA_MASTERCARD'
+        DINERS: 'DINERS',
+        IDEAL: 'IDEAL',
+        MAESTRO: 'MAESTRO',
+        MASTERPASS: 'MASTERPASS',
+        P24: 'P24',
+        VISA_MASTERCARD: 'CB_VISA_MASTERCARD'
     },
 
     /**
@@ -20,26 +23,16 @@ MangoPaySDK.card = {
         }
 
         // Cast Card to CardRegistration
-        var cr = new MangoPaySDK.card.CardRegistration(obj);
+        var cr = new MangoPaySDK.cardRegistraton.CardRegistration(obj);
 
         // Create the card registration
-        MangoPaySDK.card.createRegistration(cr, function (err, result) {
-            if (err) {
+        MangoPaySDK.cardRegistraton.create(cr, function (err, result) {
+            if (err || !result || result.Status !== MangoPaySDK.cardRegistraton.status.CREATED) {
                 handleAPIResponse(err, result);
             } else {
-                console.log('CR', result);
-
-                console.log({
-                    data: result.PreregistrationData,
-                    accessKeyRef: result.AccessKey,
-                    cardNumber: obj.CardNumber,
-                    cardExpirationDate: obj.CardExpirationDate,
-                    cardCvx: obj.CardCvx
-                });
-
                 // Send card details
                 HTTP.post(result.CardRegistrationURL, {
-                    data: {
+                    params: {
                         data: result.PreregistrationData,
                         accessKeyRef: result.AccessKey,
                         cardNumber: obj.CardNumber,
@@ -47,16 +40,12 @@ MangoPaySDK.card = {
                         cardCvx: obj.CardCvx
                     }
                 }, function (err, result2) {
-                    if (err) {
+                    if (err || !result2 || result2.statusCode !== 200 || !result2.content) {
                         handleAPIResponse(err, result2);
                     } else {
-                        console.log('RD', result2);
-
-                        var data2 = result2.data;
-
                         // Save registration data
-                        MangoPaySDK.card.update(result.Id, {
-                            RegistrationData: data2
+                        MangoPaySDK.cardRegistraton.update(result.Id, {
+                            RegistrationData: result2.content
                         }, callback);
                     }
                 });
@@ -65,31 +54,31 @@ MangoPaySDK.card = {
     },
 
     /**
-     * Creates a new card registration
-     * @param obj
+     * Fetches the card by Id
+     * @param cardId
      * @param callback
      */
-    createRegistration: function (obj, callback) {
-        if (!(obj instanceof MangoPaySDK.card.CardRegistration)) {
-            throw new Error('obj is not instance of CardRegistration');
+    fetch: function (cardId, callback) {
+        if (typeof cardId !== 'number' && typeof cardId !== 'string') {
+            throw new Error('cardId is not valid');
         }
-        HttpClient.post('/cardregistrations', obj, callback);
+        HttpClient.get('/cards/' + cardId, callback);
     },
 
     /**
      * Updates the card by Id
-     * @param id
+     * @param cardId
      * @param obj
      * @param callback
      */
-    update: function (id, obj, callback) {
-        if (typeof id !== 'number' && typeof id !== 'string') {
-            throw new Error('id is not valid');
+    update: function (cardId, obj, callback) {
+        if (typeof cardId !== 'number' && typeof cardId !== 'string') {
+            throw new Error('cardId is not valid');
         }
         if (typeof obj !== 'object' || obj === null) {
             throw new Error('obj is not valid');
         }
-        HttpClient.post('/cardregistrations/' + id, obj, callback);
+        HttpClient.put('/cards/' + cardId, obj, callback);
     },
 
     /**
@@ -106,10 +95,50 @@ MangoPaySDK.card = {
         this.UserId = null;
 
         _.extend(this, options);
+    }
+};
+
+MangoPaySDK.cardRegistraton = {
+
+    /**
+     * Status of the Card Registration
+     */
+    status: {
+        CREATED: 'CREATED',
+        ERROR: 'ERROR',
+        VALIDATED: 'VALIDATED'
     },
 
     /**
-     * A payment card
+     * Creates a new card registration
+     * @param obj
+     * @param callback
+     */
+    create: function (obj, callback) {
+        if (!(obj instanceof MangoPaySDK.cardRegistraton.CardRegistration)) {
+            throw new Error('obj is not instance of CardRegistration');
+        }
+        HttpClient.post('/cardregistrations', obj, callback);
+    },
+
+    /**
+     * Updates the card registration by Id
+     * @param cardRegistrationId
+     * @param obj
+     * @param callback
+     */
+    update: function (cardRegistrationId, obj, callback) {
+        if (typeof cardRegistrationId !== 'number' && typeof cardRegistrationId !== 'string') {
+            throw new Error('cardRegistrationId is not valid');
+        }
+        if (typeof obj !== 'object' || obj === null) {
+            throw new Error('obj is not valid');
+        }
+        HttpClient.put('/cardregistrations/' + cardRegistrationId, obj, callback);
+    },
+
+    /**
+     * A payment card registration
      * @param options
      * @constructor
      */
